c1d63d965c1406a6b389fde9a814566a34fd0b9e
BoyuanFeng <bfeng9@wisc.edu>
2022-03-11 13:57:41 -0800

    bench native msm & pairing

--- src/bench.rs
+++ src/bench.rs
@@ -3,9 +3,10 @@ extern crate test;
 #[cfg(test)]
 mod tests{
     use crate::fft::{generate_random_evaluation, compute_fft_and_ifft};
-    use ark_bls12_381::G1Affine;
+    use crate::pairing::{generate_pairing_inputs, compute_billinearity};
+    use crate::msm::{generate_msm_inputs, compute_msm};
     use super::*;
-    use test::Bencher;
+    use test::{Bencher, black_box};
 
     #[bench]
     fn bench_fft_and_ifft(b: &mut Bencher) {
@@ -13,16 +14,37 @@ mod tests{
         let output_domain_dim = 16;
         let (rand_evaluation_domain, output_domain) = generate_random_evaluation(input_domain_dim, output_domain_dim);
 
+        let rand_evaluation_domain = black_box(rand_evaluation_domain);
+        let output_domain = black_box(output_domain);
+
         b.iter( || {
             compute_fft_and_ifft(rand_evaluation_domain.clone(), output_domain);
         });
     }
 
+    #[bench]
+    fn bench_pairing(b: &mut Bencher) {
+        let size = 1<<6;
+        let (g1_rand_vec, g2_rand_vec) = generate_pairing_inputs(size);
+        
+        let g1_rand_vec = black_box(g1_rand_vec);
+        let g2_rand_vec = black_box(g2_rand_vec);
 
-    // #[bench]
-    // fn bench_msm(b: &mut Bencher) {
-    //     b.iter( || {
-    //         // test_var_base_msm::<G1Affine>();
-    //     });
-    // }
+        b.iter(|| {
+            compute_billinearity(g1_rand_vec.clone(), g2_rand_vec.clone());
+        });
+    }
+
+    #[bench]
+    fn bench_msm(b: &mut Bencher) {
+        let size = 1<<10;
+        let (point_vec, scalar_vec) = generate_msm_inputs(size);
+
+        let point_vec = black_box(point_vec);
+        let scalar_vec = black_box(scalar_vec);
+        
+        b.iter( || {
+            compute_msm(point_vec.clone(), scalar_vec.clone());
+        });
+    }
 }
--- src/msm.rs
+++ src/msm.rs
@@ -1,71 +1,35 @@
-// use ark_bls12_381::G1Affine;
-use ark_ec::{AffineCurve, ProjectiveCurve,msm::VariableBase};
-// use ark_ff::{UniformRand};
-use ark_ff::fields::PrimeField;
+use ark_ec::{msm::VariableBaseMSM, AffineCurve, ProjectiveCurve};
+use ark_ff::{PrimeField, UniformRand, Zero};
+use ark_bls12_381::G1Affine;
 
-use ark_algebra_test_templates::{
-    curves::*, fields::*, generate_field_test, generate_g1_test, groups::*, msm::*,
-};
-
-// use ark_ec::{models::SWModelParameters, AffineCurve, PairingEngine, ProjectiveCurve};
-use ark_ff::{Field, One, SquareRootField, UniformRand, Zero};
-
-use ark_bls12_381::{g1, Fq, Fq2, Fq6, Fr, G1Affine, G1Projective};
-use ark_std::{
-    ops::{AddAssign, MulAssign, SubAssign},
-    rand::Rng,
-    test_rng,
-};
-
-pub fn test_var_base_msm<G: AffineCurve>() {
-    const SAMPLES: usize = 1 << 10;
-
+pub fn generate_msm_inputs(size: usize)
+-> (
+    Vec<<<G1Affine as AffineCurve>::Projective as ProjectiveCurve>::Affine>, 
+    Vec<<<G1Affine as AffineCurve>::ScalarField as PrimeField>::BigInt>,
+){
     let mut rng = ark_std::test_rng();
-    // TODO: Not sure why into_bigint() is not found here.
-    let v = (0..SAMPLES - 1)
-        .map(|_| G::ScalarField::rand(&mut rng).into_bigint())
-        .collect::<Vec<_>>();
-    let g = (0..SAMPLES)
-        .map(|_| G::Projective::rand(&mut rng))
-        .collect::<Vec<_>>();
-    let g = <G::Projective as ProjectiveCurve>::batch_normalization_into_affine(&g);
 
-    let fast = VariableBase::msm(g.as_slice(), v.as_slice());
+    let scalar_vec = (0..size - 1)
+        .map(|_| <G1Affine as AffineCurve>::ScalarField::rand(&mut rng).into_repr())
+        .collect::<Vec<_>>();
+    let point_vec = (0..size)
+        .map(|_| <G1Affine as AffineCurve>::Projective::rand(&mut rng))
+        .collect::<Vec<_>>();
+    let point_vec = <<G1Affine as AffineCurve>::Projective as ProjectiveCurve>::batch_normalization_into_affine(&point_vec);
+
+    (point_vec, scalar_vec)
+}
+
+pub fn compute_msm(
+    point_vec: Vec<<<G1Affine as AffineCurve>::Projective as ProjectiveCurve>::Affine>,
+    scalar_vec: Vec<<<G1Affine as AffineCurve>::ScalarField as PrimeField>::BigInt>,
+) {
+    let res = VariableBaseMSM::multi_scalar_mul(point_vec.as_slice(), scalar_vec.as_slice());
 }
 
 #[test]
 fn test() {
-    // TODO: Not sure why G1Affine cannot be used here.
-    test_var_base_msm::<G1Affine>();
+    let size = 1<<10;
+    let (point_vec, scalar_vec) = generate_msm_inputs(size);
+    compute_msm(point_vec, scalar_vec);
 }
-
-// // generate_field_test!(bls12_381; fq2; fq6; mont(6, 4); );
-// generate_g1_test!(bls12_381; curve_tests; sw_tests;);
-
-
-
-// pub fn generate_random_input<G: AffineCurve>()
-// ->(Vec<_>, Vec<_>)
-// {
-//     const SAMPLES: usize = 1 << 10;
-//     let mut rng = ark_std::test_rng();
-
-//     let v = (0..SAMPLES-1)
-//         .map(|_| G::ScalarField::rand(&mut rng).into_bigint())
-//         .collect::<Vec<_>>();
-//     let g = (0..SAMPLES)
-//         .map(|_| G::Projective::rand(&mut rng))
-//         .collect::<Vec<_>>();
-//     let g = <G::Projective as ProjectiveCurve>::batch_normalization_into_affine(&g);
-
-//     (g, v)
-// }
-
-
-
-// pub fn compute_msm() {
-
-// }
-
-
-
