2d00f890a81008157b8df453cc284133cb57f9b3
BoyuanFeng <bfeng9@wisc.edu>
2022-03-17 00:05:26 -0700

    batch affine for point addition

--- src/bench.rs
+++ src/bench.rs
@@ -5,11 +5,15 @@ mod tests{
     use crate::fft::{generate_random_evaluation, compute_fft_and_ifft};
     use crate::pairing::{generate_pairing_inputs, compute_billinearity};
     use crate::msm::{generate_msm_inputs, compute_msm};
+    use crate::pippenger_msm::{mixed_point_addition, batch_affine_point_addition, batch_affine_point_addition_pre_allocate};
     use crate::stream_pippenger::ChunkedPippenger;
     use super::*;
-    use ark_bls12_381::G1Affine;
+    use ark_ec::{AffineCurve, ProjectiveCurve, short_weierstrass_jacobian::GroupProjective, short_weierstrass_jacobian::GroupAffine, bls12::Bls12Parameters};
     use test::{Bencher, black_box};
-    
+    use ark_ff::{prelude::*, Fp384};
+    use ark_std::{vec::Vec, UniformRand, rand::Rng};
+    use ark_bls12_381::{G1Affine, FqParameters};
+
     #[bench]
     fn bench_fft_and_ifft(b: &mut Bencher) {
         let input_domain_dim = 14;
@@ -64,6 +68,68 @@ mod tests{
                 p.add(g, s);
             }
             let stream_pippenger_msm = p.finalize();
+        });
+    }
+
+    #[bench]
+    fn bench_mixed_point_addition(b: &mut Bencher) {
+        let size = 1<<18;
+
+        let mut rng = ark_std::test_rng();
+        let point_vec = (0..size)
+            .map(|_| <G1Affine as AffineCurve>::Projective::rand(&mut rng))
+            .collect::<Vec<_>>();
+        let point_vec = <<G1Affine as AffineCurve>::Projective as ProjectiveCurve>::batch_normalization_into_affine(&point_vec);
+        let point_vec = black_box(point_vec);
+
+        let first_index_vec: Vec<usize> = (0..size).map(|_| rng.gen_range(0..size)).collect();
+        let second_index_vec: Vec<usize> = (0..size).map(|_| rng.gen_range(0..size)).collect();
+
+        b.iter(|| {
+            let mixed_point_addition_res = mixed_point_addition(point_vec.as_slice(), first_index_vec.as_slice(), second_index_vec.as_slice());
+        });
+    }
+
+    #[bench]
+    fn bench_batch_affine_point_addition(b: &mut Bencher) {
+        let size = 1<<18;
+
+        let mut rng = ark_std::test_rng();
+        let point_vec = (0..size)
+            .map(|_| <G1Affine as AffineCurve>::Projective::rand(&mut rng))
+            .collect::<Vec<_>>();
+        let point_vec = <<G1Affine as AffineCurve>::Projective as ProjectiveCurve>::batch_normalization_into_affine(&point_vec);
+
+        let first_index_vec: Vec<usize> = (0..size).map(|_| rng.gen_range(0..size)).collect();
+        let second_index_vec: Vec<usize> = (0..size).map(|_| rng.gen_range(0..size)).collect();
+
+        b.iter(|| {
+            let batch_affine_point_addition_res = batch_affine_point_addition(point_vec.as_slice(), first_index_vec.as_slice(), second_index_vec.as_slice());
+        });
+    }
+
+    #[bench]
+    fn bench_batch_affine_point_addition_preallocate(b: &mut Bencher) {
+        let size = 1<<14;
+
+        let mut rng = ark_std::test_rng();
+        let point_vec = (0..size)
+            .map(|_| <G1Affine as AffineCurve>::Projective::rand(&mut rng))
+            .collect::<Vec<_>>();
+        let point_vec = <<G1Affine as AffineCurve>::Projective as ProjectiveCurve>::batch_normalization_into_affine(&point_vec);
+        let point_vec = black_box(point_vec);
+
+        let first_index_vec: Vec<usize> = (0..size).map(|_| rng.gen_range(0..size)).collect();
+        let second_index_vec: Vec<usize> = (0..size).map(|_| rng.gen_range(0..size)).collect();
+
+        let mut a_vec = vec![<ark_ec::short_weierstrass_jacobian::GroupAffine<<ark_bls12_381::Parameters as Bls12Parameters>::G1Parameters> as AffineCurve>::BaseField::zero(); size];
+        let mut d_vec = vec![<ark_ec::short_weierstrass_jacobian::GroupAffine<<ark_bls12_381::Parameters as Bls12Parameters>::G1Parameters> as AffineCurve>::BaseField::one(); size];
+        let mut diff_vec = vec![<ark_ec::short_weierstrass_jacobian::GroupAffine<<ark_bls12_381::Parameters as Bls12Parameters>::G1Parameters> as AffineCurve>::BaseField::zero(); size];
+        let mut e_vec = vec![<ark_ec::short_weierstrass_jacobian::GroupAffine<<ark_bls12_381::Parameters as Bls12Parameters>::G1Parameters> as AffineCurve>::BaseField::zero(); size];
+        let mut r_vec = vec![<ark_ec::short_weierstrass_jacobian::GroupAffine<<ark_bls12_381::Parameters as Bls12Parameters>::G1Parameters> as AffineCurve>::BaseField::zero(); size];
+        
+        b.iter(|| {
+            let batch_affine_point_addition_res = batch_affine_point_addition_pre_allocate(point_vec.as_slice(), first_index_vec.as_slice(), second_index_vec.as_slice(), a_vec.as_mut_slice(), d_vec.as_mut_slice(), diff_vec.as_mut_slice(), e_vec.as_mut_slice(), r_vec.as_mut_slice());
         });
     }
 }
--- src/pippenger_msm.rs
+++ src/pippenger_msm.rs
@@ -1,10 +1,11 @@
 use std::marker::PhantomData;
 use std::ops::AddAssign;
 
-use ark_ec::short_weierstrass_jacobian::GroupAffine;
-use ark_ff::prelude::*;
+use ark_ec::{short_weierstrass_jacobian::GroupAffine, bls12::Bls12Parameters};
+use ark_ff::{prelude::*, Fp384};
 use ark_std::vec::Vec;
 use ark_ec::{AffineCurve, ProjectiveCurve, short_weierstrass_jacobian::GroupProjective};
+use ark_bls12_381::{G1Affine, FqParameters};
 
 /// The result of this function is only approximately `ln(a)`
 /// [`Explanation of usage`]
@@ -113,105 +114,256 @@ impl VariableBaseMSM {
                     total
                 })
     }
+}
 
-    /// Independent point addition with the mixed addition algorithm.
-    /// For example, for index i, we computes points[first_index_vec[i]] + points[second_index_vec[i]].
-    pub fn mixed_point_addition<G: AffineCurve>(
-        points: &[G],
-        first_index_vec: &[usize],
-        second_index_vec: &[usize],
-    ) -> Vec<G::Projective> {
-        assert_eq!(first_index_vec.len(), second_index_vec.len());
+/// Independent point addition with the mixed addition algorithm.
+/// For example, for index i, we computes points[first_index_vec[i]] + points[second_index_vec[i]].
+pub fn mixed_point_addition(
+    points: &[G1Affine],
+    first_index_vec: &[usize],
+    second_index_vec: &[usize],
+) -> Vec<<ark_ec::short_weierstrass_jacobian::GroupAffine<<ark_bls12_381::Parameters as Bls12Parameters>::G1Parameters> as AffineCurve>::Projective> {
+    assert_eq!(first_index_vec.len(), second_index_vec.len());
 
-        // Check out-of-boundary error
-        // Assume no NaN in first_index_vec and second_index_vec
-        let max_idx = first_index_vec.iter().max().unwrap();
-        assert!(*max_idx < points.len());
-        let max_idx = second_index_vec.iter().max().unwrap();
-        assert!(*max_idx < points.len());
+    // Check out-of-boundary error
+    // Assume no NaN in first_index_vec and second_index_vec
+    let max_idx = first_index_vec.iter().max().unwrap();
+    assert!(*max_idx < points.len());
+    let max_idx = second_index_vec.iter().max().unwrap();
+    assert!(*max_idx < points.len());
 
-        let zero = G::Projective::zero();
-        let mut results = vec![zero; first_index_vec.len()];
+    let zero = <ark_ec::short_weierstrass_jacobian::GroupAffine<<ark_bls12_381::Parameters as Bls12Parameters>::G1Parameters> as AffineCurve>::Projective::zero();
+    let mut results = vec![zero; first_index_vec.len()];
 
-        for i in 0..first_index_vec.len() {
-            let first_idx = first_index_vec[i];
-            let second_idx = second_index_vec[i];
-            let mut res = points[first_idx].into_projective();
-            res.add_assign_mixed(&points[second_idx]);
-            results[i] = res;
+    for i in 0..first_index_vec.len() {
+        let first_idx = first_index_vec[i];
+        let second_idx = second_index_vec[i];
+        let mut res = points[first_idx].into_projective();
+        res.add_assign_mixed(&points[second_idx]);
+        results[i] = res;
+    }
+    results
+}
+
+// Independent point addition with batch affine optimization.
+// For index i, we computes points[first_index_vec[i]] + points[second_index_vec[i]].
+// For detailed comparison against `fn mixed_point_addition(...)`, 
+//     please check doc at https://hackmd.io/@tazAymRSQCGXTUKkbh1BAg/Sk27liTW9
+pub fn batch_affine_point_addition(
+    points: &[G1Affine],
+    first_index_vec: &[usize],
+    second_index_vec: &[usize],
+) -> Vec<<ark_ec::short_weierstrass_jacobian::GroupAffine<<ark_bls12_381::Parameters as Bls12Parameters>::G1Parameters> as AffineCurve>::Projective> {
+    // Assume that all Elliptic Curve Point in points is not infinity or zero.
+    assert_eq!(first_index_vec.len(), second_index_vec.len());
+
+    // Check out-of-boundary error
+    // Assume no NaN in first_index_vec and second_index_vec
+    let max_idx = first_index_vec.iter().max().unwrap();
+    assert!(*max_idx < points.len());
+    let max_idx = second_index_vec.iter().max().unwrap();
+    assert!(*max_idx < points.len());
+
+    let size = first_index_vec.len();
+
+    // A collection of a_i = x_{i,2} - x_{i,1}
+    let mut a_vec = vec![<ark_ec::short_weierstrass_jacobian::GroupAffine<<ark_bls12_381::Parameters as Bls12Parameters>::G1Parameters> as AffineCurve>::BaseField::zero(); size];
+    let mut d_vec = vec![<ark_ec::short_weierstrass_jacobian::GroupAffine<<ark_bls12_381::Parameters as Bls12Parameters>::G1Parameters> as AffineCurve>::BaseField::one(); size];
+
+    for i in 0..size {
+        let first_idx = first_index_vec[i];
+        let second_idx = second_index_vec[i];
+        let diff = points[second_idx].x - points[first_idx].x;
+        if diff.is_zero() {
+            a_vec[i] = points[first_idx].y.double();
+        } else {
+            a_vec[i] = diff;
         }
-        results
     }
 
-    // Independent point addition with batch affine optimization.
-    // For index i, we computes points[first_index_vec[i]] + points[second_index_vec[i]].
-    // For detailed comparison against `fn mixed_point_addition(...)`, 
-    //     please check doc at https://hackmd.io/@tazAymRSQCGXTUKkbh1BAg/Sk27liTW9
-    pub fn batch_affine_point_addition<G: AffineCurve>(
-        points: &[G],
-        first_index_vec: &[usize],
-        second_index_vec: &[usize],
-    ) -> Vec<G::Projective> {
-        assert_eq!(first_index_vec.len(), second_index_vec.len());
+    for i in 1..size {
+        d_vec[i] = d_vec[i-1]*a_vec[i-1];
+    }
+    let s = (d_vec[size-1] * a_vec[size-1]).inverse().unwrap();
 
-        // Check out-of-boundary error
-        // Assume no NaN in first_index_vec and second_index_vec
-        let max_idx = first_index_vec.iter().max().unwrap();
-        assert!(*max_idx < points.len());
-        let max_idx = second_index_vec.iter().max().unwrap();
-        assert!(*max_idx < points.len());
-
-        let size = first_index_vec.len();
-
-        // A collection of a_i = x_{i,2} - x_{i,1}
-        let mut a_vec = vec![G::BaseField::zero(); size];
-        let mut d_vec = vec![G::BaseField::one(); size];
-
-        for i in 0..size {
-            let first_idx = first_index_vec[i];
-            let second_idx = second_index_vec[i];
-            a_vec[i] = points[second_idx].x - points[first_idx].x;
-        }
-
-        for i in 1..size {
-            d_vec[i] = d_vec[i-1]*a_vec[i-1];
-        }
-        let s = (d_vec[size-1] * a_vec[size-1]).inverse().unwrap();
-
-        let mut e_vec = vec![G::BaseField::zero(); size];
-        e_vec[size-1] = s;
-        for i in (0..size-1).rev() {
-            e_vec[i] = e_vec[i+1]*a_vec[i+1];
-        }
-
-        let mut r_vec = vec![G::BaseField::zero(); size];
-        let zero = G::Projective::zero();
-        let result = vec![zero; size];
-        for i in 0..size {
-            // r_vec[i] = 1/(x_{i,2} - x_{i,1})
-            r_vec[i] = d_vec[i] * e_vec[i];
-
-            let first_idx = first_index_vec[i];
-            let second_idx = second_index_vec[i];
-            let first_point = points[first_idx];
-            let second_point = points[second_idx];
-
-            let m = (second_point.y - first_point.y) * r_vec[i];
-            let x3 = m*m - first_point.x - second_point.x;
-            let y3 = first_point.x + m * (x3 - first_point.x);
-
-            let output_point = GroupAffine{
-                x: x3,
-                y: y3,
-                infinity: false,
-                _params: PhantomData,
-            };
-
-            result[i] = output_point.into();
-        }
-
-        result
+    let mut e_vec = vec![<ark_ec::short_weierstrass_jacobian::GroupAffine<<ark_bls12_381::Parameters as Bls12Parameters>::G1Parameters> as AffineCurve>::BaseField::zero(); size];
+    e_vec[size-1] = s;
+    for i in (0..size-1).rev() {
+        e_vec[i] = e_vec[i+1]*a_vec[i+1];
     }
 
+    let mut r_vec = vec![<ark_ec::short_weierstrass_jacobian::GroupAffine<<ark_bls12_381::Parameters as Bls12Parameters>::G1Parameters> as AffineCurve>::BaseField::zero(); size];
+    let zero = <ark_ec::short_weierstrass_jacobian::GroupAffine<<ark_bls12_381::Parameters as Bls12Parameters>::G1Parameters> as AffineCurve>::Projective::zero();
+    let mut result = vec![zero; size];
+    let three = <ark_bls12_381::Parameters as Bls12Parameters>::Fp::from(3);
 
+    for i in 0..size {
+        // r_vec[i] = 1/(x_{i,2} - x_{i,1})
+        r_vec[i] = d_vec[i] * e_vec[i];
+
+        let first_idx = first_index_vec[i];
+        let second_idx = second_index_vec[i];
+        let first_point = points[first_idx];
+        let second_point = points[second_idx];
+
+        let m; 
+        let diff = (second_point.x - first_point.x);
+        if diff.is_zero() {
+            // m = (3*x_1^2 + a) / (2*y1).
+            // a==0 in BLS12-381.
+            m = three * first_point.x.square() * r_vec[i];
+        } else {
+            m = (second_point.y - first_point.y) * r_vec[i];
+        }
+        let x3 = m*m - first_point.x - second_point.x;
+        let y3 = m * (first_point.x - x3) - first_point.y;
+
+        let output_point = GroupAffine::new(x3, y3, false);
+        result[i] = output_point.into();
+    }
+
+    result
+}
+
+
+// Independent point addition with batch affine optimization.
+// For index i, we computes points[first_index_vec[i]] + points[second_index_vec[i]].
+// For detailed comparison against `fn mixed_point_addition(...)`, 
+//     please check doc at https://hackmd.io/@tazAymRSQCGXTUKkbh1BAg/Sk27liTW9
+pub fn batch_affine_point_addition_pre_allocate(
+    points: &[G1Affine],
+    first_index_vec: &[usize],
+    second_index_vec: &[usize],
+    // Pre-allocate vector for a, d, e, 
+    a_vec: &mut [Fp384<FqParameters>],
+    d_vec: &mut [Fp384<FqParameters>],
+    diff_vec: &mut [Fp384<FqParameters>],
+    e_vec: &mut [Fp384<FqParameters>],
+    r_vec: &mut [Fp384<FqParameters>],
+) -> Vec<<ark_ec::short_weierstrass_jacobian::GroupAffine<<ark_bls12_381::Parameters as Bls12Parameters>::G1Parameters> as AffineCurve>::Projective> {
+    // Assume that all Elliptic Curve Point in points is not infinity or zero.
+    assert_eq!(first_index_vec.len(), second_index_vec.len());
+
+    // Check out-of-boundary error
+    // Assume no NaN in first_index_vec and second_index_vec
+    let max_idx = first_index_vec.iter().max().unwrap();
+    assert!(*max_idx < points.len());
+    let max_idx = second_index_vec.iter().max().unwrap();
+    assert!(*max_idx < points.len());
+
+    let size = first_index_vec.len();
+
+    assert!(a_vec.len() <= size, "a_vec length should be smaller than first_index_vec.len()");
+    assert!(d_vec.len() <= size, "a_vec length should be smaller than first_index_vec.len()");
+    assert!(diff_vec.len() <= size, "a_vec length should be smaller than first_index_vec.len()");
+    assert!(e_vec.len() <= size, "a_vec length should be smaller than first_index_vec.len()");
+    assert!(r_vec.len() <= size, "a_vec length should be smaller than first_index_vec.len()");
+
+    // A collection of a_i = x_{i,2} - x_{i,1}
+    for i in 0..size {
+        let first_idx = first_index_vec[i];
+        let second_idx = second_index_vec[i];
+        diff_vec[i] = points[second_idx].x - points[first_idx].x;
+        if diff_vec[i].is_zero() {
+            a_vec[i] = points[first_idx].y.double();
+        } else {
+            a_vec[i] = diff_vec[i];
+        }
+    }
+
+    for i in 1..size {
+        d_vec[i] = d_vec[i-1]*a_vec[i-1];
+    }
+    let s = (d_vec[size-1] * a_vec[size-1]).inverse().unwrap();
+
+    e_vec[size-1] = s;
+    for i in (0..size-1).rev() {
+        e_vec[i] = e_vec[i+1]*a_vec[i+1];
+    }
+
+    let zero = <ark_ec::short_weierstrass_jacobian::GroupAffine<<ark_bls12_381::Parameters as Bls12Parameters>::G1Parameters> as AffineCurve>::Projective::zero();
+    let mut result = vec![zero; size];
+    let three = <ark_bls12_381::Parameters as Bls12Parameters>::Fp::from(3);
+
+    for i in 0..size {
+        // r_vec[i] = 1/(x_{i,2} - x_{i,1})
+        r_vec[i] = d_vec[i] * e_vec[i];
+
+        assert_eq!(r_vec[i], a_vec[i].inverse().unwrap());
+        let first_idx = first_index_vec[i];
+        let second_idx = second_index_vec[i];
+        let first_point = points[first_idx];
+        let second_point = points[second_idx];
+
+        let m; 
+        if diff_vec[i].is_zero() {
+            // m = (3*x_1^2 + a) / (2*y1).
+            // a==0 in BLS12-381.
+            m = three * first_point.x.square() * r_vec[i];
+        } else {
+            m = (second_point.y - first_point.y) * r_vec[i];
+        }
+        let x3 = m.square() - first_point.x - second_point.x;
+        let y3 = m * (first_point.x - x3) - first_point.y;
+
+        let output_point = GroupAffine::new(x3, y3, false);
+        result[i] = output_point.into();
+    }
+
+    result
+}
+
+mod test {
+    use super::*;
+    use ark_std::rand::Rng;
+    
+    #[test]
+    fn test_batch_affine_correctness() {
+        let size = 1<<10;
+
+        let mut rng = ark_std::test_rng();
+        let point_vec = (0..size)
+            .map(|_| <G1Affine as AffineCurve>::Projective::rand(&mut rng))
+            .collect::<Vec<_>>();
+        let point_vec = <<G1Affine as AffineCurve>::Projective as ProjectiveCurve>::batch_normalization_into_affine(&point_vec);
+
+        let first_index_vec: Vec<usize> = (0..size).map(|_| rng.gen_range(0..size)).collect();
+        let second_index_vec: Vec<usize> = (0..size).map(|_| rng.gen_range(0..size)).collect();
+
+        let mixed_point_addition_res = mixed_point_addition(point_vec.as_slice(), first_index_vec.as_slice(), second_index_vec.as_slice());
+        let batch_affine_point_addition_res = batch_affine_point_addition(point_vec.as_slice(), first_index_vec.as_slice(), second_index_vec.as_slice());
+
+        let mixed_point_addition_res = <<G1Affine as AffineCurve>::Projective as ProjectiveCurve>::batch_normalization_into_affine(&mixed_point_addition_res);
+        let batch_affine_point_addition_res = <<G1Affine as AffineCurve>::Projective as ProjectiveCurve>::batch_normalization_into_affine(&batch_affine_point_addition_res);
+
+        assert_eq!(mixed_point_addition_res, batch_affine_point_addition_res);
+    }
+
+    #[test]
+    fn test_batch_affine_pre_allocate_correctness() {
+        let size = 1<<10;
+
+        let mut rng = ark_std::test_rng();
+        let point_vec = (0..size)
+            .map(|_| <G1Affine as AffineCurve>::Projective::rand(&mut rng))
+            .collect::<Vec<_>>();
+        let point_vec = <<G1Affine as AffineCurve>::Projective as ProjectiveCurve>::batch_normalization_into_affine(&point_vec);
+
+        let first_index_vec: Vec<usize> = (0..size).map(|_| rng.gen_range(0..size)).collect();
+        let second_index_vec: Vec<usize> = (0..size).map(|_| rng.gen_range(0..size)).collect();
+        let mut a_vec = vec![<ark_ec::short_weierstrass_jacobian::GroupAffine<<ark_bls12_381::Parameters as Bls12Parameters>::G1Parameters> as AffineCurve>::BaseField::zero(); size];
+        let mut d_vec = vec![<ark_ec::short_weierstrass_jacobian::GroupAffine<<ark_bls12_381::Parameters as Bls12Parameters>::G1Parameters> as AffineCurve>::BaseField::one(); size];
+        let mut diff_vec = vec![<ark_ec::short_weierstrass_jacobian::GroupAffine<<ark_bls12_381::Parameters as Bls12Parameters>::G1Parameters> as AffineCurve>::BaseField::zero(); size];
+        let mut e_vec = vec![<ark_ec::short_weierstrass_jacobian::GroupAffine<<ark_bls12_381::Parameters as Bls12Parameters>::G1Parameters> as AffineCurve>::BaseField::zero(); size];
+        let mut r_vec = vec![<ark_ec::short_weierstrass_jacobian::GroupAffine<<ark_bls12_381::Parameters as Bls12Parameters>::G1Parameters> as AffineCurve>::BaseField::zero(); size];
+        
+
+        let mixed_point_addition_res = mixed_point_addition(point_vec.as_slice(), first_index_vec.as_slice(), second_index_vec.as_slice());
+        let batch_affine_point_addition_res = batch_affine_point_addition_pre_allocate(point_vec.as_slice(), first_index_vec.as_slice(), second_index_vec.as_slice(), a_vec.as_mut_slice(), d_vec.as_mut_slice(), diff_vec.as_mut_slice(), e_vec.as_mut_slice(), r_vec.as_mut_slice());
+
+        let mixed_point_addition_res = <<G1Affine as AffineCurve>::Projective as ProjectiveCurve>::batch_normalization_into_affine(&mixed_point_addition_res);
+        let batch_affine_point_addition_res = <<G1Affine as AffineCurve>::Projective as ProjectiveCurve>::batch_normalization_into_affine(&batch_affine_point_addition_res);
+
+        assert_eq!(mixed_point_addition_res, batch_affine_point_addition_res);
+    }
 }
