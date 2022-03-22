25728e71411c6e0bf74a3879a1ea9ab7dccbb833
BoyuanFeng <bfeng9@wisc.edu>
2022-03-22 14:15:02 -0700

    support range of input in wasm profiling

--- src/lib.rs
+++ src/lib.rs
@@ -8,24 +8,20 @@ pub mod pairing;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn bench_fft_and_ifft() {
-    let input_domain_dim = 14;
-    let output_domain_dim = 18;
+pub fn bench_fft_and_ifft(input_domain_dim: usize, output_domain_dim: usize) {
     let (rand_evaluation_domain, output_domain) = fft::generate_random_evaluation(input_domain_dim, output_domain_dim);
 
     fft::compute_fft_and_ifft(rand_evaluation_domain.clone(), output_domain);
 }
 
 #[wasm_bindgen]
-pub fn bench_msm() {
-    let size = 1<<14;
-    let (point_vec, scalar_vec) = msm::generate_msm_inputs(size);
+pub fn bench_msm(size: usize) {
+    let (point_vec, scalar_vec) = msm::generate_msm_inputs(1<<size);
     msm::compute_msm(point_vec, scalar_vec);
 }
 
 #[wasm_bindgen]
-pub fn bench_pairing() {
-    let size = 1<<2;
-    let (g1_rand_vec, g2_rand_vec) = pairing::generate_pairing_inputs(size);
+pub fn bench_pairing(size: usize) {
+    let (g1_rand_vec, g2_rand_vec) = pairing::generate_pairing_inputs(1<<size);
     pairing::compute_billinearity(g1_rand_vec, g2_rand_vec);
 }
