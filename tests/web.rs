42d7774e2560c3dad45281c9c8572b6b679e0790
Boyuan Feng <bfeng9@wisc.edu>
2022-05-26 00:26:23 -0400

    clean

--- tests/web.rs
+++ tests/web.rs
@@ -6,19 +6,9 @@ wasm_bindgen_test_configure!(run_in_browser);
 
 static REPEAT: usize = 5;
 
-use thirtyfour::{TimeoutConfiguration, WebDriver};
-use std::time::Duration;
-
-#[test]
-fn some_print_test() {
-    let driver = WebDriver::new("http://localhost:4444/wd/hub", &caps).await?;
-    let timeouts = driver.get_timeouts().await?;
-    println!("Page load timeout = {:?}", timeouts.page_load());
-}
-
 #[wasm_bindgen_test]
 fn benchmark() {
-    for size in (8..10).step_by(2) {
+    for size in (18..20).step_by(2) {
         let point_vec = PointVectorInput::new(2 << size);
         let scalar_vec = ScalarVectorInput::new(2 << size);
         let start_time = instant::Instant::now();
